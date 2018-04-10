41f6c350248f81ee5e29513803d4178e8678680e
Iswanul Umam <umam@sepulsa.com>
2018-04-10 17:52:07 +0700

    finish half

--- composition.js
+++ composition.js
@@ -36,7 +36,7 @@ class OtherCookies extends Cookies {
   }
 }
 
-// driver code ----------------------------------
+// -----------------------------------
 
 class Ingredient {
   constructor(option) {
@@ -45,61 +45,40 @@ class Ingredient {
   }
 }
 
-// function formatter(line) {
-//   let result = {};
-//   let comp = line.split(',');
-//   for (let prop of comp) {
-//     let item = prop.trim().split(': ');
-//     result[item[1]] = item[0];
-//   }
-//   return result;
-// }
-
-// let format = formatter('1 cup: flour, 2 cups (gluten): sugar, 2 cups: peanut butter, 1 cup: cinnamon, 2 tsp: butter');
-// console.log('>>>', format);
-
-// 1 cup: flour, 2 cups (gluten): sugar, 2 cups: peanut butter, 1 cup: cinnamon, 2 tsp: butter
-
 class CookieFactory {
   constructor() {}
   static create(options) {
-
     let result = [];
     for (let row of options) {
       row = row.split(' = ');
 
       if (row[0] == 'peanut butter') {
-        let obj = {};
-        let comp = row[1].split(',');
-        for (let prop of comp) {
-          let item = prop.trim().split(': ');
-          obj[item[1]] = item[0];
-        }  
-        result.push(new PeanutButter(row[0], obj));
-
-      } else if (row[0] == 'chocolate chip') {
-        let obj = {};
-        let comp = row[1].split(',');
-        for (let prop of comp) {
-          let item = prop.trim().split(': ');
-          obj[item[1]] = item[0];
-        } 
-        result.push(new ChocolateChip(row[0], obj));
-
-      } else {
-        let obj = {};
-        let comp = row[1].split(',');
-        for (let prop of comp) {
-          let item = prop.trim().split(': ');
-          obj[item[1]] = item[0];
+        let newListItem = [];
+        let spareLine = row[1].split(',');
+        for (let eachItem of spareLine) {
+          let item = eachItem.trim().split(': ');
+          newListItem.push(new Ingredient({ name: item[1], amount: item[0] }));
         }
-        result.push(new OtherCookies(row[0], obj));
+        result.push(new PeanutButter(row[0], newListItem));
+      } else if (row[0] == 'chocolate chip') {
+        let newListItem = [];
+        let spareLine = row[1].split(',');
+        for (let eachItem of spareLine) {
+          let item = eachItem.trim().split(': ');
+          newListItem.push(new Ingredient({ name: item[1], amount: item[0] }));
+        }
+        result.push(new ChocolateChip(row[0], newListItem));
+      } else {
+        let newListItem = [];
+        let spareLine = row[1].split(',');
+        for (let eachItem of spareLine) {
+          let item = eachItem.trim().split(': ');
+          newListItem.push(new Ingredient({ name: item[1], amount: item[0] }));
+        }
+        result.push(new ChocolateChip(row[0], newListItem));
       }
     }
     return result;
-  }
-  hello() {
-    return `Hello!`;
   }
   _formatter(line) {
     let result = {};
@@ -111,6 +90,8 @@ class CookieFactory {
     return result;
   }
 }
+
+// driver code -------------------------------------------------------
 
 let options = fs.readFileSync('new_cookies.txt', 'utf8').split('\n');
 
